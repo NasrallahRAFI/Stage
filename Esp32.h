952e0dffc3c701d538134ff3b17af334cdc43ea7
Rafiabdollah <nasro.abdo@hotmail.fr>
2025-06-23 12:10:42 +0100

    Update Esp32.h
    Simple wifi connection

--- Esp32.h
+++ Esp32.h
@@ -1,1 +1,28 @@
+#include <WiFi.h>
 
+// Replace these with your WiFi credentials
+const char* ssid = "YOUR_WIFI_NAME";
+const char* password = "YOUR_WIFI_PASSWORD";
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  // Connection successful
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Your main code here
+}
